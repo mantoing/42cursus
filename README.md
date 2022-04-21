178b27b8d9bdf6d54f3498f0b9de5084ace060c5
Jaeyun Won <jaeywon@c3r3s5.42seoul.kr>
2022-04-21 19:12:52 +0900

    202204211

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 42cursus
+
+
+https://www.notion.so/42Cursus-21c950e67d914fe281ba48e4fbf9420f
--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jaeywon <jaeywon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 14:41:38 by jaeywon           #+#    #+#             */
-/*   Updated: 2022/04/14 17:05:17 by jaeywon          ###   ########.fr       */
+/*   Updated: 2022/04/21 18:18:30 by jaeywon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,3 +26,10 @@ int	ft_printf(const char *ag, ...)
 	va_end(ap);
 	return (npf);
 }
+ #include <stdio.h>
+ int main(void)
+ {
+	 ft_printf("내 나이는 %d,%z입니다.", 10);
+	 printf("내 나이는 %d,%z입니다.", 10);
+	 return (0);
+ }
