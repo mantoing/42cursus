6a7b06746c1492d936dc3f5445723353531d77c7
jaeywon <dnsjwodyd@gmail.com>
2023-04-10 16:25:31 +0900

    check cpp00

--- cpp/cpp00/ex00/megaphone.cpp
+++ cpp/cpp00/ex00/megaphone.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaeywon <jaeywon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/10 16:15:52 by jaeywon           #+#    #+#             */
+/*   Updated: 2023/04/10 16:23:54 by jaeywon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include <string>
 
@@ -7,13 +19,15 @@ int main(int ac, char **av)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 	}
-	for (int i = 1; i < ac; i++)
-	{
-		for (int j = 0; i < av[i][j]; j++)
+	else {
+		for (int i = 1; i < ac; i++)
 		{
-			std::cout << (char)toupper(av[i][j]);
+			for (int j = 0; i < av[i][j]; j++)
+			{
+				std::cout << (char)toupper(av[i][j]);
+			}
 		}
+		std::cout << std::endl;
 	}
-	std::cout << std::endl;
 	return (0);
 }
